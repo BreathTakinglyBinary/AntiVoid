f87727368d2f616db82c230bbd8c58ec90e98956
95CivicSi <aaron_salyer@yahoo.com>
2019-05-27 02:43:41 -0400

    Add option to send players back to default world spawn.

--- src/BreathTakinglyBinary/AntiVoid/AntiVoid.php
+++ src/BreathTakinglyBinary/AntiVoid/AntiVoid.php
@@ -17,9 +17,13 @@ class AntiVoid extends PluginBase implements Listener{
 	/** @var string[] */
 	private $disabledWorlds = [];
 
+	/** @var bool */
+	private $useDefaultWorld = false;
+
 	public function onEnable() : void{
 		$this->enabledWorlds = $this->getConfig()->get("enabled-worlds");
 		$this->enabledWorlds = $this->getConfig()->get("disabled-worlds");
+		$this->useDefaultWorld = $this->getConfig()->get("use-default-world");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 
@@ -32,15 +36,24 @@ class AntiVoid extends PluginBase implements Listener{
 		$level = $entity->getLevel()->getFolderName();
 		if($event->getCause() === EntityDamageEvent::CAUSE_VOID){
 			if(empty($this->enabledWorlds) and empty($this->disabledWorlds)){
-				$entity->teleport($entity->getLevel()->getSafeSpawn());
+				$this->savePlayerFromVoid($entity);
 				$event->setCancelled();
 			} elseif(in_array($level, $this->enabledWorlds) and !in_array($level, $this->disabledWorlds)){
-				$entity->teleport($entity->getLevel()->getSafeSpawn());
+				$this->savePlayerFromVoid($entity);
 				$event->setCancelled();
 			} elseif(in_array($level, $this->enabledWorlds) and !(in_array($level, $this->enabledWorlds) and in_array($level, $this->disabledWorlds))){
-				$entity->teleport($entity->getLevel()->getSafeSpawn());
+				$this->savePlayerFromVoid($entity);
 				$event->setCancelled();
 			}
 		}
+	}
+
+	private function savePlayerFromVoid(Player $player) : void{
+		if($this->useDefaultWorld){
+			$position = $player->getServer()->getDefaultLevel()->getSpawnLocation();
+		} else {
+			$position = $player->getLevel()->getSpawnLocation();
+		}
+		$player->teleport($position);
 	}
 }
